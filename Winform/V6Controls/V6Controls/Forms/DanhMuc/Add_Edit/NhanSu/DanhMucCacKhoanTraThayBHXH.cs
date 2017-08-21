33eca6a908ad780be3f727e25772a3a0113ca8c2
ptphat <ptphat1609@gmail.com>
2017-08-21 09:15:17 +0700

    tắt chức năng F8 và k cho xóa trùng mã

--- Winform/V6Controls/V6Controls/Forms/DanhMuc/Add_Edit/NhanSu/DanhMucCacKhoanTraThayBHXH.cs
+++ Winform/V6Controls/V6Controls/Forms/DanhMuc/Add_Edit/NhanSu/DanhMucCacKhoanTraThayBHXH.cs
@@ -1,8 +1,9 @@
-﻿using System.Data.SqlClient;
-using V6Structs;
-using System;
-using V6Init;
+﻿using System;
+using System.Collections.Generic;
 using V6AccountingBusiness;
+using V6Init;
+using V6Structs;
+using V6Tools;
 
 namespace V6Controls.Forms.DanhMuc.Add_Edit.NhanSu
 {
@@ -17,6 +18,32 @@ namespace V6Controls.Forms.DanhMuc.Add_Edit.NhanSu
         {
 
         }
+        public override void DoBeforeEdit()
+        {
+            try
+            {
+                IDictionary<string, object> keys = new Dictionary<string, object>();
+                keys.Add("MA_DM", TableName);
+                var aldm = V6BusinessHelper.Select(V6TableName.Aldm, keys, "*").Data;
+                string F8_table = "", code_field = "";
+
+                if (aldm.Rows.Count == 1)
+                {
+                    var row = aldm.Rows[0];
+                    F8_table = row["F8_TABLE"].ToString().Trim();
+                    //code_field = row[""].ToString().Trim();
+                }
+              
+
+                var v = Categories.IsExistOneCode_List(F8_table, "MA_TTBH", txtMaKhoan.Text);
+                txtMaKhoan.Enabled = !v;
+
+            }
+            catch (Exception ex)
+            {
+                Logger.WriteToLog("BPHT DisableWhenEdit " + ex.Message);
+            }
+        }   
         public override void ValidateData()
         {
             var errors = "";
