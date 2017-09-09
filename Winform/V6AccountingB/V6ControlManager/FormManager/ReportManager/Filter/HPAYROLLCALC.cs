b311f32f57ff662eae5b32cdc008df172d5b351e
ptphat1609 <ptphat1609@hotmail.com>
2017-09-09 12:31:56 +0700

    tính lương

--- Winform/V6AccountingB/V6ControlManager/FormManager/ReportManager/Filter/HPAYROLLCALC.cs
+++ Winform/V6AccountingB/V6ControlManager/FormManager/ReportManager/Filter/HPAYROLLCALC.cs
@@ -16,12 +16,7 @@ namespace V6ControlManager.FormManager.ReportManager.Filter
             txtNam.Value = V6Setting.M_ngay_ct2.Year;
 
 
-            Txtnh_vt1.VvarTextBox.SetInitFilter("loai_nh=1");
-            Txtnh_vt2.VvarTextBox.SetInitFilter("loai_nh=2");
-            Txtnh_vt3.VvarTextBox.SetInitFilter("loai_nh=3");
-            Txtnh_vt4.VvarTextBox.SetInitFilter("loai_nh=4");
-            Txtnh_vt5.VvarTextBox.SetInitFilter("loai_nh=5");
-            Txtnh_vt6.VvarTextBox.SetInitFilter("loai_nh=6");
+         
         }   
 
         
@@ -35,51 +30,13 @@ namespace V6ControlManager.FormManager.ReportManager.Filter
             //@Year int,
             //@nUserID int,
             //@nDubugMode int,
-       
 
+            int dubugMode = 0;
             var result = new List<SqlParameter>();
             result.Add(new SqlParameter("@nMonth", (int)txtThang.Value));
-            result.Add(new SqlParameter("@Year", (int)txtNam.Value));
+            result.Add(new SqlParameter("@nYear", (int)txtNam.Value));
             result.Add(new SqlParameter("@nUserID", V6Login.UserId));
-            result.Add(new SqlParameter("@nDubugMode", 0));
-            var and = radAnd.Checked;
-            var cKey = "";
-            var cKey_SD = "";
-
-            var key0 = GetFilterStringByFields(new List<string>()
-            {
-               "MA_KHO","MA_VT"
-            }, and);
-            var key1 = GetFilterStringByFields(new List<string>()
-            {
-                 "NH_VT1","NH_VT2","NH_VT3","NH_VT4","NH_VT5","NH_VT6","TK_VT"
-            }, and);
-
-            if (!string.IsNullOrEmpty(key0))
-            {
-                if (and)
-                {
-                    cKey += string.Format("(1=1 AND {0})", key0);
-                }
-                else
-                {
-                    cKey += string.Format("(1=2 OR {0})", key0);
-                }
-            }
-            else
-            {
-                cKey = "1=1";
-            }
-
-            if (!string.IsNullOrEmpty(key1))
-            {
-                cKey = cKey + string.Format(" and ma_vt in (select ma_vt from alvt where {0} )", key1);
-            }
-
-            cKey_SD += cKey;
-
-            result.Add(new SqlParameter("@Advance", cKey));
-            
+            result.Add(new SqlParameter("@nDubugMode", dubugMode));
             return result;
         }
 
@@ -90,13 +47,18 @@ namespace V6ControlManager.FormManager.ReportManager.Filter
                 var txt = (V6NumberTextBox) sender;
                 if (txt.Value < 1) txt.Value = 0;
                 if (txt.Value > 12) txt.Value = 12;
+                Number1 = txtThang.Value;
             }
             catch (Exception)
             {
                 
             }
+
+        }
+        private void txtNam_TextChanged(object sender, EventArgs e)
+        {
+            Number2 = txtNam.Value;
         }
 
-        
     }
 }
--- Winform/V6AccountingB/V6ControlManager/FormManager/ReportManager/XuLy/HPAYROLLCALC.cs
+++ Winform/V6AccountingB/V6ControlManager/FormManager/ReportManager/XuLy/HPAYROLLCALC.cs
@@ -4,6 +4,8 @@ using System.Data.SqlClient;
 using System.Threading;
 using V6AccountingBusiness;
 using V6Controls.Forms;
+using System.Windows.Forms;
+using V6Controls;
 using V6Init;
 using V6Tools;
 using V6Tools.V6Convert;
@@ -29,273 +31,24 @@ namespace V6ControlManager.FormManager.ReportManager.XuLy
 
         public override void SetStatus2Text()
         {
-            V6ControlFormHelper.SetStatusText2("Tính giá trung bình.");
         }
 
         protected override void Nhan()
         {
+            int thangKhoaSo;
+            int thangXuLy;
+            thangKhoaSo = V6Setting.M_Ngay_ks.Year * 12 + V6Setting.M_Ngay_ks.Month;
+            thangXuLy = (int)FilterControl.Number2 * 12 + (int)FilterControl.Number1;
+            if (thangXuLy < thangKhoaSo)
+            {
+                this.ShowWarningMessage(V6Text.CheckLock);
+                return;
+            }
             base.Nhan();
         }
 
         //Sửa lại hoàn toàn. Tách thành nhiều hàm trong Thread.
-        protected override void ExecuteProcedure()
-        {
-      
-
-            //0 @nMonth int,
-            //1 @Year int,
-            //2 @nUserID int,
-            //3 @nDubugMode int,
-            if (GenerateProcedureParameters())
-            {
-
-                var m_BigData = ObjectAndString.ObjectToString(V6Options.V6OptionValues["M_BIG_DATA"]);
-
-                if  (m_BigData == "1")
-                {
-                    var tTinhToan = new Thread(TinhGia_TB);
-                    tTinhToan.Start();
-                }
-                else
-                {
-                    var tTinhToan_All = new Thread(TinhGia_TB_All);
-                     tTinhToan_All.Start();    
-                }
-                
-
-                timerViewReport.Start();
-            }
-        }
-
-        private void TinhGia_TB()
-        {
-            try
-            {
-                _executing = true;
-
-                var ds1 = TinhGia_TB1();
-                var MaxCal = ds1.Tables[0];
-                var count = ObjectAndString.ObjectToInt(MaxCal.Rows[0][0]??0) + 2;
-
-               
-                var m_giavt=ObjectAndString.ObjectToString(V6Options.V6OptionValues["M_GIA_VT"]);
-                if (m_giavt=="1" || m_giavt=="0")
-                {
-                    count = 1;
-                }
-
-                for (int no = 0; no < count; no++)
-                {
-                    SetStatusText("Đang tính giá ... ");
-
-                    var tb2 = TinhGia_TB2().Tables[0];
-                    var row = tb2.Rows[0];
-                    var @Ngay_ct1 = ObjectAndString.ObjectToDate(row["Ngay_ct1"]);
-                    var @Ngay_ct2 = ObjectAndString.ObjectToDate(row["Ngay_ct2"]);
-                    var @Gia_vt = row["Gia_vt"];
-                    var @Ma_Kho = row["Ma_Kho"];
-                    var @Ma_vt = row["Ma_vt"];
-                    var @Tinh_giatb = row["Tinh_giatb"];
-                    var @Advance = row["Advance"].ToString().Trim();
-
-                    if (Ngay_ct1 != null)
-                        for (DateTime i = Ngay_ct1.Value; i <= Ngay_ct2; i = i.AddDays(1))
-                        {
-                            SqlParameter[] plist =
-                            {
-                                new SqlParameter("@Ngay_ct1", i), 
-                                new SqlParameter("@Ngay_ct2", i), 
-                                new SqlParameter("@Gia_vt", Gia_vt), 
-                                new SqlParameter("@Ma_Kho", Ma_Kho), 
-                                new SqlParameter("@Ma_vt", Ma_vt), 
-                                new SqlParameter("@Tinh_giatb", Tinh_giatb), 
-                                new SqlParameter("@Advance", Advance), 
-                            };
-                            SetStatusText("Đang cập nhật giá ... " + i.ToString("dd/MM/yyyy"));
-                            TinhGia_TB3(plist);
-                        }
-                }
-                
-                
-                TinhGia_TB4();
-                var Dk_cl = ObjectAndString.ObjectToInt(_pList[6].Value);
-                if (Dk_cl != 0)
-                {
-                    SetStatusText("Đang tạo chênh lệch ... ");
-                    TinhGia_TB5(_pList.ToArray());
-                }
-                
-                TinhGia_TB6();
-                SetStatusText(V6Text.Finish);
-                _success = true;
-                _executing = false;
-            }
-            catch (Exception ex)
-            {
-                _executing = false;
-                _success = false;
-                Logger.WriteToLog(GetType() + ".TinhGia_TB " + ex.Message);
-                SetStatusText("Tính lỗi: " + ex.Message);
-            }
-        }
-        private void TinhGia_TB_All()
-        {
-            try
-            {
-                _executing = true;
-
-                SetStatusText("Đang tính giá ... ");
-                var ds1 = TinhGia_TB1();
-                var MaxCal = ds1.Tables[0];
-                var count = ObjectAndString.ObjectToInt(MaxCal.Rows[0][0] ?? 0) + 2;
-
-
-                var m_giavt = ObjectAndString.ObjectToString(V6Options.V6OptionValues["M_GIA_VT"]);
-                if (m_giavt == "1" || m_giavt == "0")
-                {
-                    count = 1;
-                }
-
-                for (int no = 0; no < count; no++)
-                {
-                    var tb2 = TinhGia_TB2().Tables[0];
-                    var row = tb2.Rows[0];
-                    var @Ngay_ct1 = ObjectAndString.ObjectToDate(row["Ngay_ct1"]);
-                    var @Ngay_ct2 = ObjectAndString.ObjectToDate(row["Ngay_ct2"]);
-                    var @Gia_vt = row["Gia_vt"];
-                    var @Ma_Kho = row["Ma_Kho"];
-                    var @Ma_vt = row["Ma_vt"];
-                    var @Tinh_giatb = row["Tinh_giatb"];
-                    var @Advance = row["Advance"].ToString().Trim();
-
-                  
-                            SqlParameter[] plist =
-                            {
-                                new SqlParameter("@Ngay_ct1", Ngay_ct1), 
-                                new SqlParameter("@Ngay_ct2", Ngay_ct2), 
-                                new SqlParameter("@Gia_vt", Gia_vt), 
-                                new SqlParameter("@Ma_Kho", Ma_Kho), 
-                                new SqlParameter("@Ma_vt", Ma_vt), 
-                                new SqlParameter("@Tinh_giatb", Tinh_giatb), 
-                                new SqlParameter("@Advance", Advance), 
-                            };
-
-                            SetStatusText("Đang cập nhật giá ... ");
-                            TinhGia_TB3_All(plist);
-                      
-                }
-
-
-                TinhGia_TB4();
-                var Dk_cl = ObjectAndString.ObjectToInt(_pList[6].Value);
-                if (Dk_cl != 0)
-                {
-                    SetStatusText("Đang tạo chênh lệch.. ");
-                    TinhGia_TB5(_pList.ToArray());
-                }
-
-                TinhGia_TB6();
-                SetStatusText(V6Text.Finish);
-                _success = true;
-                _executing = false;
-            }
-            catch (Exception ex)
-            {
-                _executing = false;
-                _success = false;
-                Logger.WriteToLog(GetType() + ".TinhGia_TB " + ex.Message);
-                SetStatusText("Tính lỗi: " + ex.Message);
-            }
-        }
-
-        private void SetStatusText(string text)
-        {
-            lblStatus.Text = text;
-        }
-
-        private void TinhGia_TB6()
-        {
-            try
-            {
-                
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("TinhGia_TB6 " + ex.Message);
-            }
-        }
-
-        private void TinhGia_TB5(SqlParameter[] plist)
-        {
-            try
-            {
-                V6BusinessHelper.ExecuteProcedure("VPA_Ingia_tb_A5", plist);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("TinhGia_TB5 " + ex.Message);
-            }
-        }
-
-        private void TinhGia_TB4()
-        {
-            try
-            {
-
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("TinhGia_TB4 " + ex.Message);
-            }
-        }
-
-        private void TinhGia_TB3(SqlParameter[] plist)
-        {
-            try
-            {
-                V6BusinessHelper.ExecuteProcedure("VPA_Ingia_tb_A3", plist);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("TinhGia_TB3 " + ex.Message);
-            }
-        }
-        private void TinhGia_TB3_All(SqlParameter[] plist)
-        {
-            try
-            {
-                V6BusinessHelper.ExecuteProcedure("VPA_Ingia_tb_A3", plist);
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("TinhGia_TB3 " + ex.Message);
-            }
-        }
-
-        private DataSet TinhGia_TB2()
-        {
-            try
-            {
-                return V6BusinessHelper.ExecuteProcedure("VPA_Ingia_tb_A2", _pList.ToArray());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("TinhGia_TB2 " + ex.Message);
-            }
-        }
-
-        private DataSet TinhGia_TB1()
-        {
-            try
-            {
-                return V6BusinessHelper.ExecuteProcedure("VPA_Ingia_tb_A1", _pList.ToArray());
-            }
-            catch (Exception ex)
-            {
-                throw new Exception("TinhGia_TB1 " + ex.Message);
-            }
-        }
-
+       
         private void InitializeComponent()
         {
             this.lblStatus = new System.Windows.Forms.Label();
