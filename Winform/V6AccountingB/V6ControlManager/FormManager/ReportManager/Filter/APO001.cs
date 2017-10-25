e547343622729d7d2ac4ae082ca71dde7512219c
ptphat1609 <ptphat1609@hotmail.com>
2017-10-25 09:25:52 +0700

    add report APOO1F5

---
+++ Winform/V6AccountingB/V6ControlManager/FormManager/ReportManager/Filter/APO001.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using V6AccountingBusiness;
+using V6Init;
+
+namespace V6ControlManager.FormManager.ReportManager.Filter
+{
+    public partial class APO001 : FilterBase
+    {
+        public APO001()
+        {
+            InitializeComponent();
+            F3 = false;
+            F5 = true;
+            ViewSum = false;
+            dateNgay_ct1.Value = V6Setting.M_ngay_ct1;
+            dateNgay_ct2.Value = V6Setting.M_ngay_ct2;
+
+            String2 = "TEN_VT";
+            String1 = "MA_VT";
+
+            txtMaDvcs.VvarTextBox.Text = V6Login.Madvcs;
+
+            if (V6Login.MadvcsCount <= 1)
+            {
+                txtMaDvcs.Enabled = false;
+            }
+            chkGiamTru.Checked = true;
+            Txtnh_kh1.VvarTextBox.SetInitFilter("loai_nh=1");
+            Txtnh_kh2.VvarTextBox.SetInitFilter("loai_nh=2");
+            Txtnh_kh3.VvarTextBox.SetInitFilter("loai_nh=3");
+            Txtnh_kh4.VvarTextBox.SetInitFilter("loai_nh=4");
+            Txtnh_kh5.VvarTextBox.SetInitFilter("loai_nh=5");
+            Txtnh_kh6.VvarTextBox.SetInitFilter("loai_nh=6");
+
+            Txtnh_vt1.VvarTextBox.SetInitFilter("loai_nh=1");
+            Txtnh_vt2.VvarTextBox.SetInitFilter("loai_nh=2");
+            Txtnh_vt3.VvarTextBox.SetInitFilter("loai_nh=3");
+            Txtnh_vt4.VvarTextBox.SetInitFilter("loai_nh=4");
+            Txtnh_vt5.VvarTextBox.SetInitFilter("loai_nh=5");
+            Txtnh_vt6.VvarTextBox.SetInitFilter("loai_nh=6");
+            SetHideFields(RTien);
+        }
+        public void SetHideFields(string Loaitien)
+        {
+            if (Loaitien == "VN")
+            {
+                _hideFields = new SortedDictionary<string, string>
+                {
+                    {"TAG", "TAG"},
+                    {"STT_REC", "STT_REC"},
+                    {"STT_REC0", "STT_REC0"},
+                    {"STT_REC_TT", "STT_REC_TT"},
+                    {"MA_TT", "MA_TT"},
+                    {"MA_GD", "MA_GD"},
+                    {"PT_NT", "PT_NT"},
+                    {"TIEN_NT2", "TIEN_NT2"},
+                    {"TIEN_NT", "TIEN_NT"},
+                    {"CK_NT", "CK_NT"},
+                    {"GG_NT", "GG_NT"},
+                    {"TEN_VT2", "TEN_VT2"},
+                    {"GIA_NT2", "GIA_NT2"},
+                    {"GIA_NT21", "GIA_NT21"},
+                    {"GIA_NT", "GIA_NT"},
+                    {"THUE_NT", "THUE_NT"}
+                };
+            }
+            else
+            {
+                _hideFields.Add("TAG", "TAG");
+                _hideFields.Add("STT_REC", "STT_REC");
+                _hideFields.Add("STT_REC0", "STT_REC0");
+                _hideFields.Add("STT_REC_TT", "STT_REC_TT");
+                _hideFields.Add("MA_TT", "MA_TT");
+                _hideFields.Add("MA_GD", "MA_GD");
+
+            }
+            
+        }
+        /// <summary>
+        /// Lay cac tham so cho procedure
+        /// </summary>
+        /// <returns>cKey</returns>
+        public override List<SqlParameter> GetFilterParameters()
+        {
+            var result = new List<SqlParameter>();
+
+            V6Setting.M_ngay_ct1 = dateNgay_ct1.Value;
+            V6Setting.M_ngay_ct2 = dateNgay_ct2.Value;
+
+
+            result.Add(new SqlParameter("@ngay_ct1", dateNgay_ct1.Value.ToString("yyyyMMdd")));
+            result.Add(new SqlParameter("@ngay_ct2", dateNgay_ct2.Value.ToString("yyyyMMdd")));
+            result.Add(new SqlParameter("@giam_tru", chkGiamTru.Checked ? 1 : 0));
+
+
+
+            var and = radAnd.Checked;
+            
+            var cKey = "";
+          
+
+            var key0 = GetFilterStringByFields(new List<string>()
+            {
+                "MA_KHO","MA_DVCS","MA_BP", "MA_KH", "MA_VV", "MA_NX", "MA_VT","MA_NVIEN"
+            }, and);
+            var key1 = GetFilterStringByFields(new List<string>()
+            {
+               "NH_KH1","NH_KH2","NH_KH3","NH_KH4","NH_KH5","NH_KH6"
+            }, and);
+            var key2 = GetFilterStringByFields(new List<string>()
+            {
+               "NH_VT1","NH_VT2","NH_VT3","NH_VT4","NH_VT5","NH_VT6", "MA_QG", "MA_NSX", "TK_VT"
+            }, and);
+
+            if (!string.IsNullOrEmpty(key0))
+            {
+                if (and)
+                {
+                    cKey =  string.Format("(1=1 AND {0})", key0);
+                }
+                else
+                {
+                    cKey = string.Format("(1=2 OR {0})", key0);
+                }
+            }
+            else
+            {
+                cKey = "1=1";
+            }
+
+            if (!string.IsNullOrEmpty(key1))
+            {
+                cKey = cKey + string.Format(" and ma_kh in (select ma_kh from alkh where {0} )", key1);
+            }
+
+            if (!string.IsNullOrEmpty(key2))
+            {
+                cKey = cKey + string.Format(" and ma_vt in (select ma_vt from alvt where {0} )", key2);
+            }
+
+            result.Add(new SqlParameter("@advance", cKey));
+            result.Add(new SqlParameter("@Group", lblGroupStringVT.Text));
+
+            return result;
+        }
+
+        private void NH_KH1_Leave(object sender, EventArgs e)
+        {
+             var current = sender as TextBox;
+            if (current != null)
+            {
+                if (current.Text.Trim() == "") current.Text = "0";
+            }
+        }
+
+        private void NH_KH_TextChanged(object sender, System.EventArgs e)
+        {
+            var current = sender as TextBox;
+            if (current != null)
+            {
+                if (current.Text.Trim() == "") return;
+                foreach (Control control in groupBoxNhom.Controls)
+                {
+                    if (control != current && control.Text == current.Text)
+                    {
+                        control.Text = "0";
+                    }
+                }
+            }
+
+            String2 =
+                V6BusinessHelper.GenGroup("TEN_NH", NH_VT1.Text, NH_VT2.Text, NH_VT3.Text, NH_VT4.Text, NH_VT5.Text, NH_VT6.Text);
+            if (String2.Length > 0) String2 += ",";
+            String2 += "TEN_VT";
+
+            var paramDic = new Dictionary<string, string>
+            {
+                {"NH_VT1", NH_VT1.Text},
+                {"NH_VT2", NH_VT2.Text},
+                {"NH_VT3", NH_VT3.Text},
+                {"NH_VT4", NH_VT4.Text},
+                {"NH_VT5", NH_VT5.Text},
+                {"NH_VT6", NH_VT6.Text},
+
+                {"MA_VT", NH_MA_VT.Text},
+
+                {"NH_KH1", NH_KH1.Text},
+                {"NH_KH2", NH_KH2.Text},
+                {"NH_KH3", NH_KH3.Text},
+                {"NH_KH4", NH_KH4.Text},
+                {"NH_KH5", NH_KH5.Text},
+                {"NH_KH6", NH_KH6.Text},
+
+                {"MA_KH", NH_MA_KH.Text},
+                
+            };
+            var lblGroupStringVTText = GenGroup(paramDic);
+
+            //var lblGroupStringKHText =
+            //    V6BusinessHelper.GenGroup("NH_KH", NH_KH1.Text, NH_KH2.Text, NH_KH3.Text, NH_KH4.Text, NH_KH5.Text, NH_KH6.Text);
+
+            lblGroupStringVT.Text = lblGroupStringVTText;
+
+            String1 = lblGroupStringVT.Text;
+        }
+
+        private string GenGroup(Dictionary<string, string> names_groups)
+        {
+            var result = "";
+            var dic = new SortedDictionary<string, string>();
+            foreach (KeyValuePair<string, string> item in names_groups)
+            {
+                var groupName = item.Key;
+                var groupSort = ("00" + item.Value).Right(2);
+                if (groupSort != "00") dic[groupSort] = groupName;
+            }
+            //for (int i = 0; i < groups.Length; i++)
+            //{
+            //    var groupName = names[i];
+            //    var groupSort = groups[i];
+            //    if (groupSort != "0") dic[groupSort] = groupName;
+            //}
+            foreach (KeyValuePair<string, string> item in dic)
+            {
+                result += "," + item.Value;
+            }
+            if (result.Length > 0) result = result.Substring(1);
+            return result;
+        }
+
+        
+
+        
+        
+    }
+}
---
+++ Winform/V6AccountingB/V6ControlManager/FormManager/ReportManager/Filter/AVBCNH70AF5.cs
@@ -0,0 +1,135 @@
+﻿using System.Collections.Generic;
+using System.Data.SqlClient;
+
+namespace V6ControlManager.FormManager.ReportManager.Filter
+{
+    public partial class AVBCNH70AF5 : FilterBase
+    {
+        public AVBCNH70AF5()
+        {
+            InitializeComponent();
+            //TxtTk.SetInitFilter("tk_cn=1");
+            F3 = true;
+            F5 = false;
+           SetParentRowEvent += AARCD1_F5_SetParentRowEvent;
+           
+        }
+
+        void AARCD1_F5_SetParentRowEvent(IDictionary<string, object> row)
+        {
+            Ma_kh_filterLine.VvarTextBox.Text = "";
+            ma_vt_filterLine.VvarTextBox.Text = "";
+
+            Txtnh_kh1.VvarTextBox.Text = "";
+            Txtnh_kh2.VvarTextBox.Text = "";
+            Txtnh_kh3.VvarTextBox.Text = "";
+            Txtnh_kh4.VvarTextBox.Text = "";
+            Txtnh_kh5.VvarTextBox.Text = "";
+            Txtnh_kh6.VvarTextBox.Text = "";
+
+            Txtnh_vt1.VvarTextBox.Text = "";
+            Txtnh_vt2.VvarTextBox.Text = "";
+            Txtnh_vt3.VvarTextBox.Text = "";
+            Txtnh_vt4.VvarTextBox.Text = "";
+            Txtnh_vt5.VvarTextBox.Text = "";
+            Txtnh_vt6.VvarTextBox.Text = "";
+
+            string nhom = String1.Trim();// NH_KH1,MA_KH,MA_VT
+
+            
+            if (nhom.Contains("NH_KH1"))
+            {
+                Txtnh_kh1.VvarTextBox.Text = row["NH_KH1"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_KH2"))
+            {
+                Txtnh_kh2.VvarTextBox.Text = row["NH_KH2"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_KH3"))
+            {
+                Txtnh_kh3.VvarTextBox.Text = row["NH_KH3"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_KH4"))
+            {
+                Txtnh_kh4.VvarTextBox.Text = row["NH_KH4"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_KH5"))
+            {
+                Txtnh_kh5.VvarTextBox.Text = row["NH_KH5"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_KH6"))
+            {
+                Txtnh_kh6.VvarTextBox.Text = row["NH_KH6"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_VT1"))
+            {
+                Txtnh_vt1.VvarTextBox.Text = row["NH_VT1"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_VT2"))
+            {
+                Txtnh_vt2.VvarTextBox.Text = row["NH_VT2"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_VT3"))
+            {
+                Txtnh_vt3.VvarTextBox.Text = row["NH_VT3"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_VT4"))
+            {
+                Txtnh_vt4.VvarTextBox.Text = row["NH_VT4"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_VT5"))
+            {
+                Txtnh_vt5.VvarTextBox.Text = row["NH_VT5"].ToString().Trim();
+            }
+            if (nhom.Contains("NH_VT6"))
+            {
+                Txtnh_vt6.VvarTextBox.Text = row["NH_VT6"].ToString().Trim();
+            }
+            if (nhom.Contains("MA_VT"))
+            {
+                ma_vt_filterLine.VvarTextBox.Text = row["MA_VT"].ToString().Trim();
+            }
+            if (nhom.Contains("MA_KH"))
+            {
+                Ma_kh_filterLine.VvarTextBox.Text = row["MA_KH"].ToString().Trim();
+            }
+
+        }
+
+        /// <summary>
+        /// Lay cac tham so cho procedure
+        /// </summary>
+        /// <returns>cKey</returns>
+        public override List<SqlParameter> GetFilterParameters()
+        {
+
+            //@ngay_ct1 char(8),
+            //@ngay_ct2 char(8),
+            //@giam_tru tinyint,
+            //@advance nvarchar(max),
+            //@Group  nvarchar(max) = '',
+            //@advance2 nvarchar(max)
+
+            var result = new List<SqlParameter>();
+            result.AddRange(InitFilters);
+
+            var nhom1 = String1.Split(','); //NH_KH1,MA_KH,MA_VT
+            var listNhom = new List<string>()
+            {
+                "MA_KH",
+                "MA_VT"
+            };
+            foreach (var nhom in nhom1)
+            {
+                listNhom.Add(nhom);
+            }
+
+
+            var keyf5 = GetFilterStringByFields(listNhom, true);
+
+            result.Add(new SqlParameter("@Advance2", keyf5));
+            return result;
+        }
+
+    }
+}
